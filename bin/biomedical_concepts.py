3095b27a29d6b2c81a7b46f18101ff6ba51d8212
Darren <darren.moreland@icloud.com>
2025-08-08 15:18:02 -0400

    Updated file paths

--- bin/biomedical_concepts.py
+++ bin/biomedical_concepts.py
@@ -2,15 +2,14 @@ import json
 import pandas as pd
 
 
-
-usdm_file = "data/usdm.json"
-out_file = "data/LLZT_BiomedicalConcepts.csv"
+usdm_file = "files/usdm_sdw_v4.0.0_amendment.json"
+out_file = "LLZT_BiomedicalConcepts.csv"
 
 try:
-    with open(usdm_file, 'r') as file:
+    with open(usdm_file, "r") as file:
         usdm = json.load(file)
 except FileNotFoundError:
-    print('The input JSON file {} does not exist'.format(usdm_file))
+    print("The input JSON file {} does not exist".format(usdm_file))
 
 # debug
 # print(usdm.keys())
@@ -22,15 +21,15 @@ references = []
 codes = []
 decodes = []
 
-bcs = usdm['study']['versions'][0]['biomedicalConcepts']
+bcs = usdm["study"]["versions"][0]["biomedicalConcepts"]
 
 for b in range(0, len(bcs)):
-    ids.append(bcs[b]['id'])
-    names.append(bcs[b]['name'])
-    synonyms.append(bcs[b]['synonyms'])
-    references.append(bcs[b]['reference'])
-    codes.append(bcs[b]['code']['standardCode']['code'])
-    decodes.append(bcs[b]['code']['standardCode']['decode'])
+    ids.append(bcs[b]["id"])
+    names.append(bcs[b]["name"])
+    synonyms.append(bcs[b]["synonyms"])
+    references.append(bcs[b]["reference"])
+    codes.append(bcs[b]["code"]["standardCode"]["code"])
+    decodes.append(bcs[b]["code"]["standardCode"]["decode"])
 
 # debug
 # print(len(ids))
@@ -39,7 +38,14 @@ for b in range(0, len(bcs)):
 # print(len(codes))
 # print(len(decodes))
 
-concepts = {'id': ids, 'name': names, 'synonyms': synonyms, 'reference': references, 'code': codes, 'decode': decodes}
+concepts = {
+    "id": ids,
+    "name": names,
+    "synonyms": synonyms,
+    "reference": references,
+    "code": codes,
+    "decode": decodes,
+}
 
 # debug
 # print(concepts.keys())
